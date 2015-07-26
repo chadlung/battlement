e2960ebdd3a0756ae53717028145319babb1aa68
John Vrbanac <john.vrbanac@linux.com>
2015-07-25 19:02:12 -0500

    Changing API Version value to v1

--- battlement/resources/version.py
+++ battlement/resources/version.py
@@ -7,6 +7,6 @@ class VersionResource(common.APIResource):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
         resp.body = self.format_response_body({
-            'API Version': '1',
+            'API Version': 'v1',
             'Service Version': '0.0.1'
         })
