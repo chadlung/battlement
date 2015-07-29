7784326ac7dcb0f85af2c68ff65daa0f24055845
John Vrbanac <john.vrbanac@linux.com>
2015-07-28 20:00:10 -0500

    Adding a simple certificate task contract for plugins

--- battlement/plugins/symantec.py
+++ battlement/plugins/symantec.py
@@ -1,7 +1,33 @@
+from battlement.queue import handlers
 from battlement.plugins import ProvisionerPluginBase
 
 
 class SymantecProvisioner(ProvisionerPluginBase):
 
+    def __init__(self, db_manager):
+        self._task_handler = SymantecTaskHandler(db_manager)
+
     def name(self):
         return 'symantec'
+
+    @property
+    def task_handler(self):
+        return self._task_handler
+
+
+class SymantecTaskHandler(handlers.CertificateTaskHandler):
+
+    def issue(self, ctx, certificate_uuid, task_uuid):
+        raise NotImplementedError
+
+    def check(self, ctx, certificate_uuid, task_uuid):
+        raise NotImplementedError
+
+    def update(self, ctx, certificate_uuid, task_uuid):
+        raise NotImplementedError
+
+    def revoke(self, ctx, certificate_uuid, task_uuid):
+        raise NotImplementedError
+
+    def cancel(self, ctx, certificate_uuid, task_uuid):
+        raise NotImplementedError
--- battlement/queue/handlers.py
+++ battlement/queue/handlers.py
@@ -1,3 +1,5 @@
+import abc
+
 from oslo_log import log
 from battlement import queue
 
@@ -8,3 +10,30 @@ class EchoTaskHandler(queue.MessagingBase):
     def echo(self, ctx, *args, **kwargs):
         LOG.info('echo: {}'.format(kwargs))
         return True
+
+
+class CertificateTaskHandler(queue.MessagingBase):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, db_manager):
+        self.db = db_manager
+
+    @abc.abstractmethod
+    def issue(self, ctx, certificate_uuid, task_uuid):
+        pass
+
+    @abc.abstractmethod
+    def check(self, ctx, certificate_uuid, task_uuid):
+        pass
+
+    @abc.abstractmethod
+    def update(self, ctx, certificate_uuid, task_uuid):
+        pass
+
+    @abc.abstractmethod
+    def revoke(self, ctx, certificate_uuid, task_uuid):
+        pass
+
+    @abc.abstractmethod
+    def cancel(self, ctx, certificate_uuid, task_uuid):
+        pass
