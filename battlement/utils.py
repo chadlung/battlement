abf995d13a913dce753ba21e35e50baa02e3068e
John Vrbanac <john.vrbanac@linux.com>
2015-07-31 00:36:13 -0500

    Making recheck configurable

--- battlement/utils.py
+++ battlement/utils.py
@@ -6,5 +6,5 @@ def new_uuid():
     return str(uuid.uuid4())
 
 
-def recheck_time():
-    return datetime.utcnow() + timedelta(seconds=10)
+def recheck_time(seconds=5):
+    return datetime.utcnow() + timedelta(seconds=seconds)
